83082e1dfca49c473cd1fab418c24e5686719187
Donnell Albertie <dalbertie5@gmail.com>
2021-07-16 12:49:18 -0500

    moreforms

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
+        <h1>User Form</h1>
         <UserForm/>
     </div>
   );
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -12,24 +12,47 @@ const UserForm = (props) => {
         const newUser = { firstname, lastname, email, password, confirmpassword };
         console.log("Welcome", newUser);
     };
+    const NameValid = input => {
+        if(input.length < 2){
+            return false;
+        }
+        return true;
+    }
+    const EmailValid = input => {
+        if(input.length < 5){
+            return false;
+        }
+        return true;
+    }
+    const PasswordValid = (input, input2) => {
+        if(input == input2){
+            return true;
+        }
+        return false;
+    }
     return(
         <div className="container">
             <form onSubmit={ createUser }>
                 <div className="form-group">
                     <label>First Name: </label> 
                     <input type="text" className="form-control" onChange={ (e) => setFirstname(e.target.value) } />
+                    {NameValid(firstname) ? "" : <p className="text-danger">Name must be longer than 2 characters!</p>}
                 </div>
                 <div className="form-group">
-                    <label>Last Name: </label> 
-                    <input type="text" className="form-control" onChange={ (e) => setLastname(e.target.value) } />
+                    <label>Last Name: </label> 
+                    <input type="text" className="form-control" onChange={ (e) => setLastname(e.target.value) } />
+                    {NameValid(firstname) ? "" : <p className="text-danger">Name must be longer than 2 characters!</p>}
                 </div>
+    <form onSubmit={ (e) => e.preventDefault() }></form>
                 <div className="form-group">
                     <label>Email Address: </label> 
                     <input type="text" className="form-control" onChange={ (e) => setEmail(e.target.value) } />
+                    {EmailValid(email) ? "" : <p className="text-danger">Email must be longer than 5 characters!</p>}
                 </div>
                 <div className="form-group">
                     <label>Password: </label>
                     <input type="text" className="form-control" onChange={ (e) => setPassword(e.target.value) } />
+                    {PasswordValid(password, confirmpassword) ? "" : <p className="text-danger">Password and Conf Password must match!</p>}
                 </div>
                 <div className="form-group">
                     <label>Confirm Password: </label>
